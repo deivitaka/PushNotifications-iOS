6878ce8e4f84decb19ea60e8d20ee92a689b402d
Deivi Taka <deivitaka@yahoo.com>
2016-06-28 00:06:04 +0200

    Initial Commit

---
+++ PushNotificationsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushNotificationsApp
+//
+//  Created by Deivi Taka on 6/28/16.
+//  Copyright © 2016 Deivi Taka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
